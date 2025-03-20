51126b3afcaa9dd47353f09a48c203f769e0e85b
c4ioba <caiovictorcvm10@gmail.com>
2025-03-20 18:38:19 -0300

    Chore: Included Dto and Mapper

--- src/main/java/com/example/java10x/CadastroNinja/Ninjas/NinjaDTO.java
+++ src/main/java/com/example/java10x/CadastroNinja/Ninjas/NinjaDTO.java
@@ -1,4 +1,23 @@
 package com.example.java10x.CadastroNinja.Ninjas;
 
+import com.example.java10x.CadastroNinja.Missoes.MissaoModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaDTO {
+
+        private Long id;
+        private String nome;
+        private String email;
+        private String imgUrl;
+        private int idade;
+        private String rank;
+        private MissaoModel missao;
+
+
 }
